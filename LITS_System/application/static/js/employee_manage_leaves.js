7c6f5f9aa8aadde426ff3c883304a5b6e1bed98c
lloydgarcia77 <lloydgarcia77@gmail.com>
2020-09-28 16:28:01 +0800

    v1.9

--- LITS_System/application/static/js/employee_manage_leaves.js
+++ LITS_System/application/static/js/employee_manage_leaves.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let table =  $('#tblLeaves').DataTable({
         "order": [[ 0, "desc" ]],
         'columnDefs': [ {
-            'targets': 7, /* column index */
+            'targets': 8, /* column index */
             'orderable': false, /* true or false */ 
         }]
     }); 
--- LITS_System/application/static/js/employee_side_manage_leaves.js
+++ LITS_System/application/static/js/employee_side_manage_leaves.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let table =  $('#tblLeaves').DataTable({
         "order": [[ 0, "desc" ]],
         'columnDefs': [ {
-            'targets': 6, /* column index */
+            'targets': 7, /* column index */
             'orderable': false, /* true or false */
         }]
     });
