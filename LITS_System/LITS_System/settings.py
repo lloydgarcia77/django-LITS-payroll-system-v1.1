82e6c09a8db4cd6bcef6e328671c1bc93388f529
Lloyd Salazar Garcia <50132431+lloydgarcia77@users.noreply.github.com>
2021-11-01 14:45:54 +0800

    Update settings.py

--- LITS_System/LITS_System/settings.py
+++ LITS_System/LITS_System/settings.py
@@ -205,27 +205,7 @@ LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 
-# -------Using Gmail
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'webby8818@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Jh7*Mb4$Hj6#'
-# # EMAIL_HOST_USER = 'lloydgarcia77@gmail.com'
-# # EMAIL_HOST_PASSWORD = 'LloydQueen77'
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-# EMAIL_FROM = EMAIL_HOST_USER
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-# --------Send Grid
-# SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY') 
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-# EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-# DEFAULT_FROM_EMAIL = 'lloydgarcia77@gmail.com'
-# EMAIL_FROM = 'lloydgarcia77@gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
 
 # Go Daddy
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
